f2de9688502f9eb434544380a4d7024edb549303
Derick Yang <derick.yang@tufts.edu>
2016-10-14 23:51:41 -0400

    added map

--- app/app.js
+++ app/app.js
@@ -1,21 +1,39 @@
 'use strict'
-
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-])
-.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('')
-
-  $routeProvider.otherwise({redirectTo: '/'})
-}])
-.controller('MainCtrl', ['$route', '$routeParams', '$location',
-  function MainCtrl ($route, $routeParams, $location) {
-    this.$route = $route
-    this.$location = $location
-    this.$routeParams = $routeParams
-    this.thing = 'dsafdsa'
-}])
+        'ngRoute',
+        'myApp.view1',
+        'myApp.view2',
+        'myApp.version',
+        'uiGmapgoogle-maps'
+    ])
+    .config(['uiGmapGoogleMapApiProvider', function (GoogleMapApi) {
+      GoogleMapApi.configure({
+        key: 'AIzaSyBI2B2hZzmW7_NcuQww8GzopJyQrddOBHs',
+        libraries: 'weather, geometry, visualization'
+      })
+    }])
+    // .config(['$locationProvider', '$routeProvider', 'uiGmapGoogleMapApiProvider', function($locationProvider, $routeProvider, uiGmapGoogleMapApiProvider) {
+    //     $locationProvider.hashPrefix('')
+    //     // uiGmapGoogleMapApiProvider.configure({
+    //     //         //    key: 'your api key',
+    //     //         v: '3.20', //defaults to latest 3.X anyhow
+    //     //         libraries: 'weather,geometry,visualization'
+    //     //     })
+    //         // $routeProvider.otherwise({redirectTo: '/'})
+    // }])
+    .controller('ListCtrl', ['$route', '$routeParams', '$location', '$scope',
+        function ListCtrl($route, $routeParams, $location, $scope) {
+            this.$route = $route
+            this.$location = $location
+            this.$routeParams = $routeParams
+            $scope.thing
+        }
+    ])
+    // derick was here
+    .controller('MapCtrl', ['$scope', 'uiGmapGoogleMapApi', function MapCtrl($scope, uiGmapGoogleMapApi) {
+        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 }
+        uiGmapGoogleMapApi.then(function(maps) {
+          console.log(maps)
+        })
+    }])
